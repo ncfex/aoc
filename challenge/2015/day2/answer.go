56d997c19efd42d081b50b76855d63f6d34179a6
ncfex <oguzhanguldibi@gmail.com>
2024-10-17 22:08:35 +0300

    complete part2

--- challenge/2015/day2/answer.go
+++ challenge/2015/day2/answer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -19,18 +20,20 @@ func main() {
 
 	dimensions := strings.Split(strings.TrimSpace(string(content)), "\n")
 	wrapperCh := make(chan int, len(dimensions))
+	ribbonCh := make(chan int, len(dimensions))
 
 	for _, line := range dimensions {
 		wg.Add(1)
 		go func(l string) {
 			defer wg.Done()
-			calculateBoxPaper(l, wrapperCh)
+			calculateBoxPaper(l, wrapperCh, ribbonCh)
 		}(string(line))
 	}
 
 	go func() {
 		wg.Wait()
 		close(wrapperCh)
+		close(ribbonCh)
 	}()
 
 	total := 0
@@ -38,10 +41,15 @@ func main() {
 		total += area
 	}
 
-	fmt.Println(total)
+	totalRibbon := 0
+	for area := range ribbonCh {
+		totalRibbon += area
+	}
+
+	fmt.Println(total, totalRibbon)
 }
 
-func calculateBoxPaper(line string, wpCh chan<- int) {
+func calculateBoxPaper(line string, wpCh chan<- int, rbCh chan<- int) {
 	dimensions := strings.Split(line, "x")
 	w, err1 := strconv.Atoi(dimensions[0])
 	l, err2 := strconv.Atoi(dimensions[1])
@@ -49,6 +57,7 @@ func calculateBoxPaper(line string, wpCh chan<- int) {
 	if err1 != nil || err2 != nil || err3 != nil {
 		log.Printf("Error parsing dimensions")
 		wpCh <- 0
+		rbCh <- 0
 	}
 
 	s1 := w * l
@@ -57,7 +66,14 @@ func calculateBoxPaper(line string, wpCh chan<- int) {
 	smallest := min(s1, min(s2, s3))
 
 	totalSurface := 2*s1 + 2*s2 + 2*s3 + smallest
+
+	arr := []int{w, l, h}
+	sort.Ints(arr)
+	smallestP, secondSmallestP := arr[0], arr[1]
+	totalRibbon := (smallestP*2 + secondSmallestP*2) + (w * h * l)
+
 	wpCh <- totalSurface
+	rbCh <- totalRibbon
 }
 
 func min(a, b int) int {
