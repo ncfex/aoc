38e5a31aa9019bcf1eeaa78b506dc91db6fce2fe
oguzhang <oguzhanguldibi@gmail.com>
2024-10-21 00:17:13 +0300

    complete d5 p2

--- challenge/2015/day5/answer.go
+++ challenge/2015/day5/answer.go
@@ -37,11 +37,13 @@ func main() {
 		wg.Add(1)
 		go func(wrd string) {
 			defer wg.Done()
-			r1 := hasEnoughVowels(wrd)
-			r2 := hasBannedSeq(wrd)
-			r3 := hasRecurringLetter(wrd)
+			// r1 := hasEnoughVowels(wrd)
+			// r2 := hasBannedSeq(wrd)
+			// r3 := hasRecurringLetter(wrd)
+			r4 := hasPairAndRepeat(wrd)
 
-			if r1 && !r2 && r3 {
+			// if r1 && !r2 && r3 { // pt1
+			if r4 {
 				resultCh <- true
 			} else {
 				resultCh <- false
@@ -92,4 +94,33 @@ func hasRecurringLetter(word string) bool {
 		}
 	}
 	return false
+}
+
+func hasPairAndRepeat(word string) bool {
+	pairMap := make(map[string]int)
+	hasPair := false
+	hasRepeat := false
+
+	for i := 0; i < len(word)-1; i++ {
+		// pair
+		pair := word[i : i+2]
+		if firstPos, ok := pairMap[pair]; ok {
+			if i-firstPos >= 2 { // overlap check
+				hasPair = true
+			}
+		} else {
+			pairMap[pair] = i
+		}
+
+		// repeat
+		if i < len(word)-2 && word[i] == word[i+2] {
+			hasRepeat = true
+		}
+
+		if hasPair && hasRepeat {
+			return true
+		}
+	}
+
+	return hasPair && hasRepeat
 }
